162629c992d6a1345b11f871a63d9add3ff3f7ca
JWhitney <jwjwhitney@gmail.com>
2017-05-03 09:44:34 -0600

    Modification working

--- app/main.js
+++ app/main.js
@@ -1,5 +1,12 @@
-﻿angular.module('app', ['ngResource'])
-    .controller('Main', function ($scope, $resource, $filter) {
+﻿var app = angular.module('app', ['ngResource']);
+
+app.factory('NewItemF', ['Resource', function ($resource) {
+    return $resource('https://msucs491-spring17-assignment2.azurewebsites.net/api/item/'), {
+        'create' : {method: 'POST'},
+    };
+}]);
+
+    app.controller('Main', function ($scope, $resource, $filter) {
 
         $scope.notes = [];
         $scope.item = null;
@@ -24,6 +31,7 @@
         var AllItemService = $resource('https://msucs491-spring17-assignment2.azurewebsites.net/api/item/',
         {
             'query': { method: 'GET', isArray: false },
+            'save': { method: 'POST' },
         });
         var AllLocationService = $resource('https://msucs491-spring17-assignment2.azurewebsites.net/api/location/',
         {
@@ -34,7 +42,21 @@
         $scope.notes.push("Fetching Item ID 363");
 
         $scope.allItems = AllItemService.get();
-        $scope.allLocations = AllLocationService.get();
+        $scope.allLocations = AllLocationService.get(function () {
+            //console.log("Hello");
+            
+            //var location;
+            /*for (let location in $scope.allLocations.Items) {
+                //console.log(location.Name+" "+location.PageIndex);
+                console.log(location);
+                $scope.addLocById(location.ID, location.Name);
+            }*/
+            $scope.allLocations.Items.forEach(function (location) {
+                console.log(location);
+                $scope.addLocById(location.ID, location.Name);
+            });
+            $scope.addRevLoc();
+        });
         $scope.item = ItemService.get({ itemId: 363 }, function () {
             $scope.notes.push("Updating Item ID 363");
             $scope.item.Quantity = $scope.item.Quantity + 1; //Increment by 1
@@ -44,7 +66,7 @@
 
         $scope.getColor = function(expVal)
         {
-            if (expVal in ['Expires in 3 days', 'Expires in 2 days', 'Expires tomorrow']) {
+            if (expVal.includes('Expires in 3 days') || expVal.includes('Expires in 2 days') || expVal.includes('Expires tomorrow')) {
                 return "yellow";
             } else if (expVal.includes('Expired')) {
                 return "red";
@@ -55,30 +77,57 @@
 
         $scope.$watch("newItem.Name", function (newVal, oldVal) {
             if (newVal == "Jack" || newVal == "jack") {
-                alert('I don\'t know that I would consider Jack to be food');
+                console.log('I don\'t know that I would consider Jack to be food');
             } else if (newVal == "Andy" || newVal == "andy") {
-                alert('I don\'t know that I would consider Andy to be food');
+                console.log('I don\'t know that I would consider Andy to be food');
             }
         });
 
         $scope.addNewItem = function()
         {
-            $scope.item = NewItemService.get({ itemId: 363 }, function () {
-                $scope.item.ID = $scope.counter;
-                $scope.item.Name = $scope.newItem.Name;
-                $scope.item.Description = $scope.newItem.Description;
-                $scope.item.Expiration = $scope.newItem.Expiration;
-                $scope.item.Location = $scope.locIdDict[$scope.newItem.Location];
-                $scope.item.Quantity = $scope.newItem.Quantity;
-                $scope.item.$save(); //Save
+            $scope.item.ID = $scope.counter;
+            $scope.item.Name = $scope.newItem.Name;
+            $scope.item.Description = $scope.newItem.Description;
+            $scope.item.Expires = new Date($scope.newItem.Expires);
+            $scope.item.Created = new Date();
+            $scope.item.LocationID = $scope.locIdDict[$scope.newItem.Location.trim()];
+            $scope.item.Quantity = $scope.newItem.Quantity;
+            AllItemService.save($scope.item, function () {
+                $scope.allItems = AllItemService.get();//update list
+                $scope.counter = $scope.counter + 1;
             });
-            
-            $scope.allItems = AllItemService.get();//update list
-            $scope.counter = $scope.counter + 1;
         }
+        $scope.modItem = function (itemNum) {
+            if (itemNum == null) {
+                alert("You must specify and item to modify");
+                return;
+            }
+            $scope.item = ItemService.get({ itemId: itemNum }, function () {
+                $scope.item.ID = itemNum;
+                $scope.item.Name = $scope.modItem.Name || $scope.item.Name;
+                $scope.item.Description = $scope.modItem.Description || $scope.item.Description;
+                $scope.item.Expires = $scope.modItem.Expires || $scope.item.Expires;
+                $scope.item.Location = $scope.locIdDict[$scope.modItem.Location] || $scope.item.Location;
+                $scope.item.Quantity = $scope.modItem.Quantity || $scope.item.Quantity;
+                ItemService.save($scope.item, function () {
+                    $scope.allItems = AllItemService.get();//update list
+                });
+            });
+        }
+
         $scope.addLocById = function (idNum, idName) {
             $scope.locDict[idNum] = idName;
-            $scope.locIdDict[idName] = idNum;
+            //console.log(idNum+", "+$scope.locDict[idNum]);
+        }
+        $scope.addRevLoc = function () {
+            for (var key in $scope.locDict) {
+                //if ($scope.locDict.hasOwnProperty(key)) {
+                    $scope.locIdDict[$scope.locDict[key]] = key;
+                //}
+            }
+            console.log("revLoc");
+            console.log("TEST");
+            console.log($scope.locIdDict);
         }
         $scope.myFormatDate = function (date) {
             let d = new Date(date);
@@ -90,7 +139,7 @@
             */
             var datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0" + (d.getMonth() + 1)).slice(-2) + "-" +
                 d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
-            return datestring;
+            return d.toDateString();
         }
         $scope.getLocFromId = function(givenLocId)
         {
@@ -108,7 +157,7 @@
                 return "No expiration date specified";
             }
 
-            let daysUntil = parseInt((t2 - t1) / (24 * 3600 * 1000));
+            let daysUntil = parseInt(Math.ceil((t2 - t1) / (24 * 3600 * 1000)));
             if (daysUntil > 0) {
                 if (daysUntil == 1) {
                     return "Expires tomorrow"
